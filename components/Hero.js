269292a9c9e7dff7cb9bbee038c5a6efeee72e94
otto <otmankhess@gmail.com>
2022-05-04 16:42:17 +0000

    second

--- components/Hero.js
+++ components/Hero.js
@@ -19,7 +19,7 @@ function Hero() {
           <AnimatedModel />
         </Canvas>
 
-        <nav className="inline-block fixed left-28 top-32 ">
+        <nav className="hidden lg:inline-block fixed left-28 top-32 ">
             <div className="absolute left-50 transform -translate-x-1/2 space-y-6">
             <a className='cursor-pointer selected-circle nav-dot h-7 w-7 block rounded-full border-4   hover:bg-green-400'></a>
             <a className='cursor-pointer nav-dot h-7 w-7 block rounded-full border-4   hover:bg-green-400 active:bg-green-700'></a>
