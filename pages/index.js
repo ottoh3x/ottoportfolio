3e1899ca47b6fb3b9cca27ab4c5e8c8e58a8868f
otto <otmankhess@gmail.com>
2022-09-14 23:12:14 +0100

    optimized

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Services from "../components/Services";
-import Landing from "./home";
 import { useEffect, useState } from "react";
 import Scroll from "../components/Scroll";
 export default function Home() {
