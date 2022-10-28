27cddfbe5add625457a80f9a9fad0580dd661508
otto <otmankhess@gmail.com>
2022-10-28 16:57:11 +0100

    bien

--- pages/bein.js
+++ pages/bein.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactPlayer from "react-player";
 
 function Bein() {
-  return <div>hello otto</div>;
+  return <div>
+    
+  <video width="1200" height="800" controls>
+  <source src="https://e17.e-amzn-cdn.net/kmxsshbnpnumwmesrlseynxvn5a4biliw6zv7r3rdp3f6sq7iufvaipdgg2q/v.mp4" type="video/mp4">
+</video>
+
+
+  </div>;
 }
 
 export default Bein;
