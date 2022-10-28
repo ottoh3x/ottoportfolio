025a56483d49948db42f597930406222b9288131
otto <otmankhess@gmail.com>
2022-10-28 17:01:03 +0100

    nb

--- pages/bein.js
+++ pages/bein.js
@@ -5,7 +5,7 @@ function Bein() {
   return <div>
     
   <video width="1200" height="800" controls>
-  <source src="https://e17.e-amzn-cdn.net/kmxsshbnpnumwmesrlseynxvn5a4biliw6zv7r3rdp3f6sq7iufvaipdgg2q/v.mp4" type="video/mp4">
+  <source src="https://e17.e-amzn-cdn.net/kmxsshbnpnumwmesrlseynxvn5a4biliw6zv7r3rdp3f6sq7iufvaipdgg2q/v.mp4" type="video/mp4" /> 
 </video>
 
 
