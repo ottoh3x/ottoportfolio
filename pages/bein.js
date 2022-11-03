82495e393f4c5520302aa825a7b3669586f43588
Othmane Khess <otmankhess@gmail.com>
2022-11-03 14:47:17 +0100

    test

--- pages/bein.js
+++ pages/bein.js
@@ -11,20 +11,26 @@ function Bein() {
  
   {ep:8,link:"https://e16.e-amzn-cdn.net/kmxstcrrpnumwmesrlseymnbhpb5dyokne5yzrvm5b6fdxlhl6nu3bffj6na/v.mp4"
   },
-  {ep:9,link:"https://e16.e-amzn-cdn.net/kmxstorrpnumwmesrlseyyfjhiir5ddt47oyb66tpt2mkcdp3wmb43invjka/v.mp4"
+  {ep:9,link:"https://vidhd.fun/embed-a0317bdran86.html"
   },
-  {ep:10,link:"https://c43.e-amzn-cdn.net/kmxsuajrpnumwmesrlseym5ip2p3fqy2nmr4alyd3r7mn42g6nojreel2rna/v.mp4"
+  {ep:10,link:"https://vidhd.fun/embed-21wf0oz30wdk.html"
   },
-  {ep:11,link:"https://e16.e-amzn-cdn.net/kmxstbrxpnumwmesrlsey6plhyt54njpxgjyixsmmawkttzzmuwb5suhkuuq/v.mp4"
+  {ep:11,link:"https://vidhd.fun/embed-xr77e7j4790y.html"
   },
-  {ep:12,link:"https://c43.e-amzn-cdn.net/kmxsvljxpnumwmesrlsey2pphcu6kf7ve5yh4yevm4idleljrwgs4gtsxwha/v.mp4"
+  {ep:12,link:"https://vidhd.fun/embed-hv1l267q90zu.html"
   },
-  {ep:13,link:"https://c32.e-amzn-cdn.net/kmxst5zxpnumwmesrlsey25kp6liwtl2ltkigwqf36d3tqiynmceujjviqla/v.mp4"
+  {ep:13,link:"https://vidhd.fun/embed-j3v4s6qmmzzy.html"
   },
-  {ep:14,link:"https://c40.e-amzn-cdn.net/kmxssprxpnumwmesrlsey4h6nptnft7yqbfd2t2wzn4idpvyvr4xtafnz45a/v.mp4"
+  {ep:14,link:"https://vidhd.fun/embed-qgblv2zmkfqb.html"
   },
-  {ep:15,link:"https://e18.e-amzn-cdn.net/kmxss5jxpnumwmesrlsey3vnm3okxyhc3pdus66tyywjgifyn4al4bxtas4q/v.mp4"
-  }
+  {ep:15,link:"https://vidhd.fun/embed-o4oizgle67ru.html"
+  },
+  {ep:16,link:"https://vidhd.fun/embed-71l5xyueo9qm.html"
+},
+{ep:17,link:"https://vidhd.fun/embed-vi1nzbqc39at.html"
+},
+{ep:18,link:"https://vidhd.fun/embed-79okb90slxjd.html"
+}
 
   ]
 
@@ -55,7 +61,8 @@ function Bein() {
   <h1 className="font-bold p-1 text-white text-center text-3xl">Episode {episode}</h1>
     <div className="p-1  ">
     
-    <Plyr source={videoSrc} />
+    {/* <Plyr source={videoSrc} /> */}
+    <iframe src={epLink} width="800" height="512" allowFullScreen sandbox ></iframe>
   
 </div>
 
