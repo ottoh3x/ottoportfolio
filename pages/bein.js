aa7f6894213409c505976fd87d3d5f8f808b8401
Othmane Khess <otmankhess@gmail.com>
2022-11-04 16:10:46 +0100

    test

--- pages/bein.js
+++ pages/bein.js
@@ -6,22 +6,22 @@ import "plyr-react/plyr.css"
 function Bein() {
 
   const [episode,setEpisode] = useState(2)
-  const [epLink,setEpLink] = useState("https://e16.e-amzn-cdn.net/kmxstcrrpnumwmesrlseymnbhpb5dyokne5yzrvm5b6fdxlhl6nu3bffj6na/v.mp4")
+  const [epLink,setEpLink] = useState("https://e18.e-amzn-cdn.net/kmxsttbupnumwmesrlseyzxaheksevrgdkrnifuxfldj3gsdffnjv2b45i5a/v.mp4")
   const episodes = [
  
-  {ep:8,link:"https://e16.e-amzn-cdn.net/kmxstcrrpnumwmesrlseymnbhpb5dyokne5yzrvm5b6fdxlhl6nu3bffj6na/v.mp4"
+  {ep:19,link:"https://e18.e-amzn-cdn.net/kmxsttbupnumwmesrlseyzxaheksevrgdkrnifuxfldj3gsdffnjv2b45i5a/v.mp4"
   },
-  {ep:9,link:"https://e16.e-amzn-cdn.net/kmxstorrpnumwmesrlseyyfjhiir5ddt47oyb66tpt2mksd4sefsxw6y3jiq/v.mp4"
+  {ep:20,link:"https://e18.e-amzn-cdn.net/kmxstyjupnumwmesrlsey3h6gaxyhdc5b4ub3oz6qfwcehryy3qgondgp46q/v.mp4"
   },
-  {ep:10,link:"https://c43.e-amzn-cdn.net/kmxsuajrpnumwmesrlseym5ip2p3fqy2nmr4alyd3r7mnm2vx7h74cc5bydq/v.mp4"
+  {ep:21,link:"https://c21.e-amzn-cdn.net/kmxstgz3pnumwmesrlsey2x3p2n4nd5r2auprxo26csdrfiouq5icpj2r7zq/v.mp4"
   },
-  {ep:11,link:"https://e16.e-amzn-cdn.net/kmxstbrxpnumwmesrlsey6plhyt54njpxgjyixsmmawktdzkfg7rlskqe4aq/v.mp4"
+  {ep:22,link:"https://e9.e-amzn-cdn.net/kmxsttr3pnumwmesrlseyyxdoeuavxomfuxtqdsqsv7jbgh46c2z7uu47jdq/v.mp4"
   },
-  {ep:12,link:"https://c43.e-amzn-cdn.net/kmxsvljxpnumwmesrlsey2pphcu6kf7ve5yh4yevm4idlul2yepdh5g526da/v.mp4"
+  {ep:23,link:"https://e9.e-amzn-cdn.net/kmxssab3pnumwmesrl5uyonmnw2wrejvbtlinzwoelekwo5lbc2qitbutiia/v.mp4"
   },
-  {ep:13,link:"https://c32.e-amzn-cdn.net/kmxst5zxpnumwmesrlsey25kp6liwtl2ltkigwqf36d3taile6l5ohjfganq/v.mp4"
+  {ep:24,link:"https://c30.e-amzn-cdn.net/kmxstdb2pnumwmesrl5uy5vbnwmuddpakazzly4mcrqmspbv32yk5uznw4ma/v.mp4"
   },
-  {ep:14,link:"https://c40.e-amzn-cdn.net/kmxssprxpnumwmesrlsey4h6nptnft7yqbfd2t2wzn4id7vl4dvg73oea7uq/v.mp4"
+  {ep:25,link:"https://c43.e-amzn-cdn.net/kmxstjz2pnumwmesrl5uyzhkhonfedouukw2bqblq3bemfdvrwgmddumxuna/v.mp4"
   },
   {ep:15,link:"https://e18.e-amzn-cdn.net/kmxss5jxpnumwmesrlsey3vnm3okxyhc3pdus66tyywjgyfleoj5uhbibmeq/v.mp4"
   },
@@ -36,6 +36,7 @@ function Bein() {
 
   const videoSrc = {
   type: "video",
+  autoPlay:true,
   sources: [
     {
       src: epLink,
@@ -58,20 +59,20 @@ function Bein() {
 
 
   return <div className="bg-[#111]">
-  <h1 className="font-bold p-1 text-white text-center text-3xl">Episode {episode}</h1>
+  <h1 className="font-bold p-1 text-white text-center text-3xl">EP {episode}</h1>
     <div className="p-1  ">
     
-    <Plyr source={videoSrc} />
+    <Plyr source={videoSrc} autoPlay playing  />
   
 </div>
 
 
-  <div className="text-gray-200 grid grid-cols-5 gap-2 p-4 container mx-auto">
+  <div className="text-gray-200 grid grid-cols-8 gap-2 p-4 container mx-auto">
    {episodes.map((ep,index) => (
     <span 
       onClick={() => setEpisode(ep.ep)} 
       key={index} 
-      className="py-2 px-4 bg-[#222] cursor-pointer rounded-lg text-center hover:bg-[#2224] border-[1px] border-stone-700">Episode {ep.ep}</span>
+      className="py-2 px-4 bg-gray-200 text-black font-bold cursor-pointer rounded-lg text-center hover:bg-[#2224] border-[1px] border-stone-700">EP {ep.ep}</span>
     ))}
 
   </div>
