011e73bc7bceaca21b693cf8098301d7d3367cb7
Othmane Khess <otmankhess@gmail.com>
2023-07-30 18:24:00 +0200

    lol

--- components/Hero.js
+++ components/Hero.js
@@ -3,17 +3,27 @@ import Image from "next/image";
 import { SiGithub, SiTwitter, SiInstagram, SiFacebook } from "react-icons/si";
 import Header from "./Header";
 import {motion} from "framer-motion"
+import Lottie from "react-lottie-player";
+import animationData from "../lotties/person-coding.json";
 
 function Hero() {
  
-
+// lottie config
+const defaultOptions = {
+  loop: true,
+  play: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
   
   return (
     
     <section className=" border-b-2 css-selector   border-gray-600 pb-4 " id="hero">
       <Header />
       <div className="container relative mt-0 lg:mt-[-5rem] h-screen flex items-center mx-auto justify-between">
-        
+      
         <div className="flex absolute bottom-0 w-full place-content-center space-x-4">
           <div className="cursor-pointer text-gray-200 hover:brightness-150 hover:scale-[1.20] transition-all ease">
             <SiGithub size={32} />
@@ -32,7 +42,7 @@ function Hero() {
         
           <motion.div initial={{opacity:0}} animate={{opacity:1}}  className="w-full">
             <h1 className="text-center text-4xl p-4 	 lg:text-7xl font-black  text-gray-400">
-              <span  className="text-gray-200">Design</span>. <span className="text-blue-600">Build</span>. <span className="text-gray-200">Improve</span>
+              <span  className="text-gray-200">I Design</span>, <span className="text-blue-600">Build</span> & <span className="text-gray-200">Improve</span>
             </h1>
             <p className="text-gray-400 text-center text-xl p-2">
               I create beautiful websites your users will love.
@@ -45,7 +55,9 @@ function Hero() {
         
         
 
-        
+          <div className="relative z-index-[5] h-[90%] w-[85%]">
+          <Lottie {...defaultOptions} />
+        </div> 
       </div>
     </section>
     
